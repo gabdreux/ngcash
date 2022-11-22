72b2cc46f3b24031f23f02f9b14ab21185ec73ae
gabriel dreux <dreuxgabriel@hotmail.com>
2022-11-22 20:45:14 -0300

    crud01

--- service/src/routes/routes.ts
+++ service/src/routes/routes.ts
@@ -1,44 +1,93 @@
 import express, { Request, Response } from 'express';
-
-
-
-const user = [{ id: "1", userName: "Nome Teste", account: "1", status: false }];
-
 const toDoRoutes = express.Router();
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient();
+
+// const user01 = [{ id: "1", userName: "Nome Teste", password: "minhaSenha", status: false, accountId: "1" }];
 
 
 
-interface user {
+// interface user { 
 
-    id: number;
-    userName: string;
-    account: number;
+//     id: number;
+//     userName: string;
+//     password: string;
+//     status: Boolean;
+//     accountId: number;
 
-};
+// };
 
 
-// CRUD
+
 
 // Create
 
-toDoRoutes.post("/user", (req: any, res: any) => {
+toDoRoutes.post("/user", async (req: any, res: any) => {
 
-    const { id, userName, account} = req.body;
-    user.push({ id, userName, account, status: false });
+    const { userName, password } = req.body;
+
+
+    const user = await prisma.user.create({
+        data: {
+            
+            userName,
+            password,
+            status: false,
+
+        },
+    });
+
     return res.status(201).json(user);
 
 
 });
 
 
-//Read
+// Read
 
-toDoRoutes.get("/user", (req: any, res: any) => {
+toDoRoutes.get("/user", async (req: any, res: any) => {
+
+    const users = await prisma.user.findMany()
+    return res.status(200).json(users);
+
+});
+
+
+// Update
+
+toDoRoutes.put("/user", async (req, res) => {
+
+    const {id, userName, status} = req.body;
+
+    if(!id){
+        return res.status(400).json("Id is not valid!")
+    };
+
+    const userAlreadyExist = await prisma.user.findUnique({
+        where: {
+            id,
+        }
+    });
+
+    if(!userAlreadyExist){
+        return res.status(404).json("Esse usuário não existe!")
+    }
+
+    const user = await prisma.user.update ({
+
+        where: {
+            id,
+        },
+        data: {
+            userName,
+            status: true,
+        },
+    });
 
     return res.status(200).json(user);
 
 });
-
 
 
 module.exports = toDoRoutes;
